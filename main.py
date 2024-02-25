f7440d3958c47a96e9dc7d3a1faf4ef717287fb9
ihjuan3 <jugopeis2001@gmail.com>
2024-02-24 20:38:12 -0800

    done

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
+    celcius = int(input('enter temperature: '))
+    fahrenheit = (1.8*celcius) + 32
 
+    print(f'Fahrenheit: {fahrenheit: .2f}')
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return celsius, fahrenheit
+    return celcius, fahrenheit
 
 
 if __name__ == '__main__':
